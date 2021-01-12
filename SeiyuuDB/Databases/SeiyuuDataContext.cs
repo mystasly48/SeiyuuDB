14c3c6351d03b740d011f077f6a7765cfcb7dc5a
Maisu <6381045+mystasly48@users.noreply.github.com>
2021-01-13 02:38:05 +0900

    DataContext 追加（なぜか stage されていなかった）

---
+++ SeiyuuDB/Databases/SeiyuuDataContext.cs
@@ -0,0 +1,21 @@
+﻿using SeiyuuDB.Entities;
+using System.Data;
+using System.Data.Linq;
+
+namespace SeiyuuDB.Databases {
+  public class SeiyuuDataContext : DataContext {
+    public Table<Actor> Actors;
+    public Table<Anime> Animes;
+    public Table<AnimeFilmography> AnimeFilmographies;
+    public Table<Company> Companies;
+    public Table<ExternalLink> ExternalLinks;
+    public Table<Game> Games;
+    public Table<GameFilmography> GameFilmographies;
+    public Table<Note> Notes;
+    public Table<OtherFilmography> OtherFilmographies;
+    public Table<Radio> Radios;
+    public Table<RadioFilmography> RadioFilmographies;
+
+    public SeiyuuDataContext(IDbConnection connection) : base(connection) { }
+  }
+}
