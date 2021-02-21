bf5cbcfeaf75661994c0733d7a792b65671aa0b4
Maisu <6381045+mystasly48@users.noreply.github.com>
2021-02-21 20:01:59 +0900

    声優タブにアニメ出演履歴を追加

---
+++ SeiyuuDB.Wpf/Models/AnimeFilmographyModel.cs
@@ -0,0 +1,37 @@
+﻿using SeiyuuDB.Wpf.Utils;
+using System.Windows;
+
+namespace SeiyuuDB.Wpf.Models {
+  public class AnimeFilmographyModel : Observable {
+    private string _title;
+    public string Title {
+      get => _title;
+      set => SetProperty(ref _title, value);
+    }
+
+    private int _releasedYear;
+    public int ReleasedYear {
+      get => _releasedYear;
+      set => SetProperty(ref _releasedYear, value);
+    }
+
+    private string _characterName;
+    public string CharacterName {
+      get => _characterName;
+      set => SetProperty(ref _characterName, value);
+    }
+
+    private bool _isMainRole;
+    public bool IsMainRole {
+      get => _isMainRole;
+      set {
+        SetProperty(ref _isMainRole, value);
+        OnPropertyChanged(nameof(CharacterFontWeight));
+      }
+    }
+
+    public FontWeight CharacterFontWeight {
+      get => IsMainRole ? FontWeights.UltraBold : FontWeights.Normal;
+    }
+  }
+}
--- SeiyuuDB.Wpf/ViewModels/ActorTabViewModel.cs
+++ SeiyuuDB.Wpf/ViewModels/ActorTabViewModel.cs
@@ -1,59 +1,19 @@
 ﻿using SeiyuuDB.Core.Entities;
-using SeiyuuDB.Core.Helpers;
+using SeiyuuDB.Wpf.Models;
 using SeiyuuDB.Wpf.Utils;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Input;
-using System.Windows.Media.Imaging;
 
 namespace SeiyuuDB.Wpf.ViewModels {
   public class ActorTabViewModel : Observable {
-    private Actor _actor;
-    public Actor Actor {
+    private ActorModel _actorModel;
+    public ActorModel ActorModel {
       get {
-        return _actor;
+        return _actorModel;
       }
       set {
-        SetProperty(ref _actor, value);
-        OnPropertyChanged(Name);
-        OnPropertyChanged(NameRomaji);
-        OnPropertyChanged(NameKana);
-        OnPropertyChanged(Nickname);
-        OnPropertyChanged(Gender);
-        OnPropertyChanged(Birthdate);
-        OnPropertyChanged(BloodType);
-        OnPropertyChanged(Height);
-        OnPropertyChanged(Hometown);
-        OnPropertyChanged(DebutYear);
-        OnPropertyChanged(SpouseName);
-        OnPropertyChanged(AgencyName);
+        SetProperty(ref _actorModel, value);
       }
     }
-
-    public string Name => Actor.Name;
-    public string NameRomaji => Actor.NameRomaji;
-    public string NameKana => Actor.NameKana;
-    public string Nickname => Actor.Nickname;
-    public string Gender => EnumHelper.DisplayName(Actor.Gender);
-    public string Birthdate {
-      get {
-        string res = Actor.Birthdate.ToString();
-        if (Actor.Birthdate.Age.HasValue) {
-          res += "（" + Actor.Birthdate.Age.Value + "歳）";
-        }
-        return res;
-      }
-    }
-    public string BloodType => EnumHelper.DisplayName(Actor.BloodType);
-    public string Height => Actor.Height.HasValue ? Actor.Height.Value.ToString() + "cm" : "";
-    public string Hometown => Actor.Hometown;
-    public string DebutYear => Actor.DebutYear.HasValue ? Actor.DebutYear.Value.ToString() + "年" : "";
-    public string SpouseName => Actor.SpouseName;
-    public string AgencyName => Actor.Agency?.Name; // ボタンにする
-    public BitmapImage Picture => string.IsNullOrEmpty(Actor.PictureUrl) ? ImageHelper.NoImage : ImageHelper.UrlToBitmapImage(Actor.PictureUrl);
 
     private bool _isCompleted;
     public bool IsCompleted {
@@ -83,7 +43,7 @@ namespace SeiyuuDB.Wpf.ViewModels {
     public ICommand DeleteActorCommand => new AnotherCommandImplementation(ExecuteDeleteActor);
 
     private void ExecuteEditInformation(object obj) {
-      //var form = new AddActorWindow(Actor);
+      //var form = new AddActorWindow(ActorModel.Actor);
       //form.Owner = Window.GetWindow(this);
       //form.ShowDialog();
       //if (form.Success) {
@@ -112,7 +72,7 @@ namespace SeiyuuDB.Wpf.ViewModels {
     }
 
     public ActorTabViewModel(Actor actor) {
-      Actor = actor;
+      ActorModel = new ActorModel() { Actor = actor };
     }
   }
 }
