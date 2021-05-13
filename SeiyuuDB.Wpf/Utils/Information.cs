c617b612d52fcf672a6cbb3624e1fe7c431a7f6a
Maisu <6381045+mystasly48@users.noreply.github.com>
2021-05-13 23:36:20 +0900

    db ファイルパス修正

--- SeiyuuDB.Wpf/Utils/Information.cs
+++ SeiyuuDB.Wpf/Utils/Information.cs
@@ -5,8 +5,8 @@ namespace SeiyuuDB.Wpf.Utils {
   public static class Information {
     public static string Title { get => "SeiyuuDB"; }
 
-    public static string SqliteFilePath => @"C:\Programming\Csharp\WPF\SeiyuuDB\Databases\SeiyuuDB.db";
-    public static string BlobFolderPath => @"C:\Programming\Csharp\WPF\SeiyuuDB\Databases\Images\";
+    public static string SqliteFilePath => @"D:\Drive\Programming\Csharp\WPF\SeiyuuDB\Databases\SeiyuuDB.db";
+    public static string BlobFolderPath => @"D:\Drive\Programming\Csharp\WPF\SeiyuuDB\Databases\Images\";
 
     public static string SettingsFolder { get => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); }
     public static string SettingsFile { get => Path.Combine(SettingsFolder, Title + ".xml"); }
