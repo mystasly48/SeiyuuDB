5185e493d2643f91aa9400210c07f970bfd2db90
Maisu <6381045+mystasly48@users.noreply.github.com>
2021-06-06 19:48:03 +0900

    非負整数のバリデーション追加

---
+++ SeiyuuDB.Wpf/Utils/PositiveIntegerValidationRule.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace SeiyuuDB.Wpf.Utils {
+  public class PositiveIntegerValidationRule : ValidationRule {
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo) {
+      bool parseResult = int.TryParse((value ?? "").ToString(), out int res);
+      if (parseResult && 0 <= res) {
+        return ValidationResult.ValidResult;
+      } else if (!parseResult) {
+        return new ValidationResult(false, "数値を入力");
+      } else {
+        return new ValidationResult(false, "非負整数を入力");
+      }
+    }
+  }
+}
